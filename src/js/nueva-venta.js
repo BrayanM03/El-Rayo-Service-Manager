a02f678e7e6c6c4e2adb91a94d8e187a86f7e8fd
BrayanM03 <natsu5679@gmail.com>
2021-01-25 22:53:18 -0600

    home

--- src/js/nueva-venta.js
+++ src/js/nueva-venta.js
@@ -1,13 +1,16 @@
 
 const inputs = document.querySelectorAll(".input-group");
+const selects = document.querySelectorAll(".select-group");
 
-inputs.forEach( input => {
+
+inputs.forEach( input => {  
 
     input.onfocus = function(){
         input.previousElementSibling.classList.add('top');
         input.previousElementSibling.classList.add('focus');
-        input.parentNode.classList.add('focus');
+        input.parentNode.classList.add('focus');  
     }
+
 
     input.onblur = function(){
         input.value = input.value.trim();
@@ -18,6 +21,31 @@ inputs.forEach( input => {
         input.previousElementSibling.classList.remove('focus');
         input.parentNode.classList.remove('focus');
     }
+});
+
+console.log(selects);
+selects.forEach( select => {
+
+    select.onfocus = function(){
+        alert("EN foco");
+       /* select.previousElementSibling.classList.add('top');
+        select.previousElementSibling.classList.add('focus');
+        select.parentNode.classList.add('focus');  */
+    }
+
+
+    select.onblur = function(){
+        
+      /*  var indice = select.selectedIndex;
+        if (indice == null) {
+            select.previousElementSibling.classList.remove('top');
+        }else{
+            select.previousElementSibling.classList.remove('focus');
+        select.parentNode.classList.remove('focus');*/
+        alert("Sin");
+        }
+        
+        
 });
 
     
