f573b6cc882335b711ec4e9bb1571ce7121e50bd
BrayanM03 <natsu5679@gmail.com>
2021-03-11 02:56:29 -0600

    Home

--- modelo/agregar-llanta-inv-total.php
+++ modelo/agregar-llanta-inv-total.php
@@ -7,11 +7,30 @@
         echo "maaaaal";
     }
 
-    if (isset($_POST["modelo"])) {
-      $respuesta =  $_POST["modelo.rin"];
+    if (isset($_POST)) {
+      $respuesta =  $_POST["mayorista"];
       
 
-      print_r("Hay conexion " . $respuesta);
+
+      $query = "INSERT INTO llantas (Ancho, Proporcion, Diametro, Descripcion, Marca, Modelo, precio_Inicial, precio_Venta, precio_Mayoreo, Fecha) VALUES (?,?,?,?,?,?,?,?,?,?)";
+      $resultado = $con->prepare($query);
+      $resultado->bind_param(
+          'iissssiiis',
+          $_POST['ancho'],
+          $_POST['alto'],
+          $_POST['rin'],
+          $_POST['descripcion'],
+          $_POST['marca'],
+          $_POST['modelo'],
+          $_POST['costo'],
+          $_POST['precio'],
+          $_POST['mayorista'],
+          $_POST['fecha'],
+
+      );
+
+      $resultado->execute();
+      print_r(1);
         
     }else{
         print_r("Error al conectar");
--- src/js/inventario-total.js
+++ src/js/inventario-total.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
         { title: "Costo",          data: "precio_Inicial" },
         { title: "Precio",         data: "precio_Venta"   },
         { title: "Precio Mayoreo", data: "precio_Mayoreo" },
+        { title: "Fecha",          data: "Fecha"          },
         { title: "Imagen",         data: "Marca", render: function(data,type,row) {
           return '<img src="./src/img/logos/'+ data +'.jpg" style="width: 60px; border-radius: 8px">';
           }},
@@ -65,7 +66,7 @@ function agregarLLanta() {
        '<div class="col-8">'+
        '<div class="form-group">'+
        '<label><b>Marca:</b></label></br>'+
-       '<select class="form-control " id="marca" value="" name="marca"></select>'+
+       '<select class="form-control" id="marca" name="marca"></select>'+
           '</div>'+
           '</div>'+
        '</div>'+
@@ -74,7 +75,7 @@ function agregarLLanta() {
         '<div class="col-4">'+
         '<div class="form-group">'+
         '<label for="ancho"><b>Ancho:</b></label></br>'+
-        '<input type="number" class="form-control" id="ancho" value="" name="ancho" placeholder="Ancho" autocomplete="off">'+
+        '<input type="number" class="form-control" id="ancho"  name="ancho" placeholder="Ancho" autocomplete="off">'+
 
 
    ' </div>'+
@@ -84,7 +85,7 @@ function agregarLLanta() {
    '<div class="col-4">'+
     '<div class="form-group">'+
     '<label><b>Alto:</b></label></br>'+
-    '<input type="number" value="" name="alto" id="alto" class="form-control" placeholder="Proporcion">'+
+    '<input type="number" name="alto" id="alto" class="form-control" placeholder="Proporcion">'+
     '</div>'+
     '</div>'+
 
@@ -92,7 +93,7 @@ function agregarLLanta() {
         '<div class="col-4">'+
         '<div class="form-group">'+
         '<label><b>Rin</b></label>'+
-        '<input type="text" class="form-control" value=""  id="rin" name="rin" placeholder="Diametro">'+
+        '<input type="text" class="form-control"  id="rin" name="rin" placeholder="Diametro">'+
     '</div>'+
         '</div>'+
 
@@ -125,7 +126,7 @@ function agregarLLanta() {
 '<div class="col-4">'+
         '<div class="form-group">'+
         '<label><b>Mayorista</b></label>'+
-        '<input type="number" class="form-control" value="" name="mayorista" di="mayorista" placeholder="0.00">'+
+        '<input type="number" class="form-control" value="" name="mayorista" id="mayorista" placeholder="0.00">'+
     '</div>'+
 '</div>'+
         '</div>'+
@@ -141,7 +142,7 @@ function agregarLLanta() {
     '<div class="col-6">'+
         '<div class="form-group">'+
             '<label><b>Cantidad</b></label>'+
-            '<input type="number" class="form-control" value=""name="cantidad" id="cantidad" placeholder="0">'+
+            '<input type="number" class="form-control" name="cantidad" id="cantidad" placeholder="0">'+
         '</div>'+
     '</div>'+
     '</div>'+
@@ -162,6 +163,8 @@ function agregarLLanta() {
     showConfirmButton: true,
     confirmButtonText: 'Actualizar', 
     cancelButtonColor:'#ff764d',
+    focusConfirm: false,
+    iconColor : "#36b9cc",
     didOpen: function () {
      
         $(document).ready(function() { 
@@ -246,35 +249,178 @@ function agregarLLanta() {
 
         });
     } ,
-  }).then(function () {  
-      marcaLLanta = $("#select2-marca-container").text();  
-      
-      datos = $("#agregar-llanta-inv-total").serialize();
-      model = $("#modelo").val();
-     
-    $.ajax({
-        type: "POST",
-        url: "./modelo/agregar-llanta-inv-total.php",
-        data: { 'marca': marcaLLanta, 'modelo': datos},
-        cache: false,
-        success: function(response) {
-            Swal.fire(
-            "¡Correcto!",
-            "El servidor respondio " + response,
+    showLoaderOnConfirm: true,
+    preConfirm: (respuesta) =>{
 
+      data = {
+        "marca":          $("#select2-marca-container").text(),  
+        "ancho":          $("#ancho").val(),
+        "alto":           $("#alto").val(),
+        "rin":            $("#rin").val(),
+        "costo":          $("#costo").val(),
+        "precio":         $("#precio").val(),
+        "mayorista":      $("#mayorista").val(),
+        "modelo":         $("#modelo").val(),
+        "fecha":          $("#fecha").val(),
+        "descripcion":    $("#descripcion").val()
+      };
+
+      if(data["marca"] == "Selecciona una marca"){
+        /*const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+        
+        Toast.fire({
+          icon: 'error',
+          title: 'Falta poner la marca'
+        })*/
+        $(".datoVacio").removeClass("datoVacio");
+        $(".select2-container").addClass("datoVacio");
+        Swal.showValidationMessage(
+          `Selecciona una marca`
+        )
+      }else if( data["ancho"] == ""){
+        $(".datoVacio").removeClass("datoVacio");
+        $(".border-danger").removeClass("border-danger");
+        $("#ancho").addClass("border-danger");
+        Swal.showValidationMessage(
+          `Establece un ancho`
+        )
+      }else if(data["alto"] == ""){
+        $(".datoVacio").removeClass("datoVacio");      
+        $(".border-danger").removeClass("border-danger");
+        $("#alto").addClass("border-danger");
+        Swal.showValidationMessage(
+          `Establece un alto`
+        )
+      }else if( data["rin"] == ""){
+        $(".datoVacio").removeClass("datoVacio");      
+        $(".border-danger").removeClass("border-danger");
+        $("#rin").addClass("border-danger");
+        Swal.showValidationMessage(
+          `Establece el rin`
+        )
+      }else if( data["modelo"] == ""){
+        $(".datoVacio").removeClass("datoVacio");      
+        $(".border-danger").removeClass("border-danger");
+        $("#modelo").addClass("border-danger");
+        Swal.showValidationMessage(
+          `Establece un modelo`
+        )
+      }else if(data["costo"] == ""){
+        $(".datoVacio").removeClass("datoVacio");      
+        $(".border-danger").removeClass("border-danger");
+        $("#costo").addClass("border-danger");
+        Swal.showValidationMessage(
+          `Establece el precio que te costó la llanta`
+        )
+      }else if( data["precio"] == ""){
+        $(".datoVacio").removeClass("datoVacio");      
+        $(".border-danger").removeClass("border-danger");
+        $("#precio").addClass("border-danger");
+        Swal.showValidationMessage(
+          `Establece un precio`
+        )
+      }else if(data["mayorista"] == ""){
+        $(".datoVacio").removeClass("datoVacio");      
+        $(".border-danger").removeClass("border-danger");
+        $("#mayorista").addClass("border-danger");
+        Swal.showValidationMessage(
+          `Establece un precio de mayorista o descuento`
+        )
+      }else if(data["fecha"] == ""){
+        $(".datoVacio").removeClass("datoVacio");      
+        $(".border-danger").removeClass("border-danger");
+        $("#fecha").addClass("border-danger");
+        Swal.showValidationMessage(
+          `Establece una fecha`
+        )
+      }else if( data["cantidad"] == ""){
+        $(".datoVacio").removeClass("datoVacio");      
+        $(".border-danger").removeClass("border-danger");
+        $("#cantidad").addClass("border-danger");
+        Swal.showValidationMessage(
+          `Establece una descripcion`
+        )
+      }else if( data["descripcion"] == ""){
+        $(".datoVacio").removeClass("datoVacio");      
+        $(".border-danger").removeClass("border-danger");
+        $("#descripcion").addClass("border-danger");
+        Swal.showValidationMessage(
+          `Establece una descripcion`
+        )
+      }
+    }
+    //Si el resultado es OK tons:
+  }).then((result) => {  
+
+   if(result.isConfirmed){
+
+    data = {
+      "marca":          $("#select2-marca-container").text(),  
+      "ancho":          $("#ancho").val(),
+      "alto":           $("#alto").val(),
+      "rin":            $("#rin").val(),
+      "costo":          $("#costo").val(),
+      "precio":         $("#precio").val(),
+      "mayorista":      $("#mayorista").val(),
+      "modelo":         $("#modelo").val(),
+      "fecha":          $("#fecha").val(),
+      "descripcion":    $("#descripcion").val()
+    };
+
+    console.log(data["marca"]);  
+
+    $.ajax({
+      type: "POST",
+      url: "./modelo/agregar-llanta-inv-total.php",
+      data:data,
+      cache: false,
+      success: function(response) {
+        if (response==1) {
+          Swal.fire(
+            "¡Correcto!",
+            "Se agrego la llanta",
             "success"
             )
-
             console.log(response);
-        },
-        failure: function (response) {
-            Swal.fire(
-            "Error",
-            "La llanta no fue agregada.", // had a missing comma
+        }else{
+          Swal.fire(
+            "¡Erro!",
+            "No se agrego la llanta",
             "error"
             )
+            console.log(response);
         }
-    });
+          
+
+          
+      },
+      failure: function (response) {
+          Swal.fire(
+          "Error",
+          "La llanta no fue agregada.", // had a missing comma
+          "error"
+          )
+      }
+  });
+    
+
+    
+   }
+
+   
+
+     
+   
 }, 
 function (dismiss) {
   if (dismiss === "cancel") {
