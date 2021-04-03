5d0a5a554ae617771047d32b1a5726d9a1fc891f
BrayanM03 <natsu5679@gmail.com>
2021-04-03 14:02:53 -0500

    oficina

--- modelo/ventas/insertar-venta.php
+++ modelo/ventas/insertar-venta.php
@@ -28,30 +28,45 @@ if(isset($_POST)){
       $count = count($codigos);
       
       $suma = array_sum($cantidades);
-      echo $suma;
+      
 
-      if($sucursal = "Sendero")
+
    
-    $queryInsertar = "INSERT INTO ventas (id, Fecha, id_Sucursal, id_Usuarios, id_Cliente, Cantidad, Total, estatus) VALUES (null,?,?,?,?,?,?,?)";
+    /*$queryInsertar = "INSERT INTO ventas (id, Fecha, id_Sucursal, id_Usuarios, id_Cliente, Cantidad, Total, estatus) VALUES (null,?,?,?,?,?,?,?)";
     $resultado = $con->prepare($queryInsertar);
     $resultado->bind_param('ssiiids', $fecha, $sucursal, $idUser, $cliente, $suma ,$total, $estatus);
     $resultado->execute();
-    $resultado->close();
-
-    print_r(1);
-
-     
-
-
-    // usar esta consulta:  select * from ventas ORDER BY id DESC LIMIT 1 para escoger el ultimo id
-
-    /*$queryInsertar = "INSERT INTO detalle_venta (id, id_Venta, id_Llanta, Cantidad, Unidad, precio_Unitario, Importe) VALUES (null,?,?,?,?,?,?,?)";
-    $resultado = $con->prepare($query);
-    $resultado->bind_param('sisi', $code, $codigo, $sucursal,$stock );
-    $resultado->execute();
     $resultado->close();*/
 
+    $sql = "SELECT id FROM ventas ORDER BY id DESC LIMIT 1";
+    $resultado = mysqli_query($con, $sql);
+    
+    if(!$resultado){
+      echo "no se pudo realizar la consulta";
+
+    }else{
+      while ($dato=mysqli_fetch_assoc($resultado)) {
+        $arreglo[] = $dato;
+
+        while ($codigo_llanta <= count($codigo_llanta)) {
+         echo "CUakc";
+        }
+
+    /*usar esta consulta:  select * from ventas ORDER BY id DESC LIMIT 1 para escoger el ultimo id
+
+          $queryInsertar = "INSERT INTO detalle_venta (id, id_Venta, id_Llanta, Cantidad, Unidad, precio_Unitario, Importe) VALUES (null,?,?,?,?,?,?,?)";
+          $resultado = $con->prepare($query);
+          $resultado->bind_param('sisi',$dato, $code, $codigo, $sucursal,$stock );
+          $resultado->execute();
+          $resultado->close();*/
+
    // echo json_encode($codigo_llanta, JSON_UNESCAPED_UNICODE);
+
+    }
+
+    print_r($arreglo[0]);
+  }
+  
 }
 
 
