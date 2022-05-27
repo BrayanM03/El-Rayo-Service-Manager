29dfeb93bd73507d42f258d69f2e9d3eecbb10d4
BrayanM03 <natsu5679@gmail.com>
2022-05-27 00:03:00 -0500

    modif nuevo cliente

--- modelo/creditos/actualizar-estatus-creditos-vencidos.php
+++ modelo/creditos/actualizar-estatus-creditos-vencidos.php
@@ -16,11 +16,43 @@
     $id_usuario = $_SESSION["id_usuario"];
     $rol = $_SESSION["rol"];
 
-    $fecha_hoy = date("Y-m-d");
+    $fecha_hoy = date("Y-m-d");    
 
     $estatusvencido = 4;
     $res =0.00;
-    $update = "UPDATE `creditos` SET estatus = ? WHERE pagado <> total AND restante <> ? AND DATE(fecha_final) <= ?";
+
+    //Codifo para cancelar credito con ventas canceladas
+    /*   $treaer = mysqli_query($con, "SELECT * FROM ventas WHERE estatus = 'Cancelada'");
+
+      while ($fila = $treaer->fetch_assoc()){
+          $id_vent = $fila["id"];
+
+          $contar = "SELECT COUNT(*) FROM creditos WHERE id_venta = ?";
+          $response = $con->prepare($contar);
+          $response->bind_param('s', $id_vent);
+          $response->execute();
+          $response->bind_result($total_matches);
+          $response->fetch();
+          $response->close();
+
+          if($total_matches > 0){
+
+            $update = "UPDATE creditos SET estatus = 5 WHERE id_venta= ?";
+            $result = $con->prepare($update);
+            $result->bind_param('s', $id_vent);
+            $result->execute();
+            $result->close();
+
+            $data[] = array("id"=>$id_vent, "OK"=>true);
+          }else{
+              $data[] = array("id"=>$id_vent, "OK"=>false);
+          }
+
+      } 
+
+      echo json_encode($data, JSON_UNESCAPED_UNICODE);*/
+
+    $update = "UPDATE creditos SET estatus = ? WHERE estatus <> 5 AND pagado <> total AND restante <> ? AND fecha_final <= ?";
     $result = $con->prepare($update);
     $result->bind_param('sss', $estatusvencido, $res, $fecha_hoy);
     $result->execute();
--- src/js/creditos.js
+++ src/js/creditos.js
@@ -10,7 +10,7 @@ function actualizarCreditoVencido() {
       console.log(response);
     }
   });
-
+ 
 }
 
 actualizarCreditoVencido();
