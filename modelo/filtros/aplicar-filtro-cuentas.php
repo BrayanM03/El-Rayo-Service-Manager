ea9e3a3e53fd728b66aa88eee522b36788b55a77
BrayanM03 <natsu5679@gmail.com>
2024-01-09 15:09:49 -0600

    Ajuste filtro de cuentas

---
+++ modelo/filtros/aplicar-filtro-cuentas.php
@@ -0,0 +1,157 @@
+<?php
+
+include '../conexion.php';
+$con= $conectando->conexion(); 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+if (!$con) {
+    echo "Problemas con la conexion";
+}
+//Funcion que sanitiza entradas
+$sanitizacion = function($valor, $con){
+    $valor_ = $con->real_escape_string($valor);
+    $valor_str = "'".$valor_."'";
+    return $valor_str;
+};
+
+$fecha_inicial = empty($_POST['fecha_inicial']) ? '' : $_POST['fecha_inicial'];
+$fecha_final = empty($_POST['fecha_final']) ? '' : $_POST['fecha_final'];
+$sucursal_ubicacion = empty($_POST['sucursal_ubicacion']) ? '' : $_POST['sucursal_ubicacion'];
+$sucursal_destino = empty($_POST['sucursal_destino']) ? '' : $_POST['sucursal_destino'];
+$proveedor = empty($_POST['filtro_proveedor']) ? '' : $_POST['filtro_proveedor'];
+$cliente = empty($_POST['cliente']) ? '' : $_POST['cliente'];
+$folio = empty($_POST['folio']) ? '' : $_POST['folio'];
+$factura = empty($_POST['factura']) ? '' : $_POST['factura'];
+$marca_llanta = empty($_POST['marca_llanta']) ? '' : $_POST['marca_llanta']; //Multiple values
+$ancho_llanta = empty($_POST['ancho_llanta']) ? '' : $_POST['ancho_llanta']; //Multiple values
+$alto_llanta = empty($_POST['alto_llanta']) ? '' : $_POST['alto_llanta']; //Multiple values
+$rin_llanta = empty($_POST['rin_llanta']) ? '' : $_POST['rin_llanta']; //Multiple values
+$tipo = empty($_POST['filtro_tipo']) ? '' : $_POST['filtro_tipo']; //Multiple values
+$estatus = empty($_POST['filtro_estatus']) ? '' : $_POST['filtro_estatus']; //Multiple values
+$estado_factura = empty($_POST['filtro_estado']) ? '' : $_POST['filtro_estado']; //Multiple values
+// Define la consulta SQL base
+$sql = "SELECT DISTINCT m.* FROM vista_movimientos m 
+LEFT JOIN usuarios u ON m.id_usuario = u.id LEFT JOIN proveedores p ON m.proveedor_id = p.id";
+
+// Filtra por marcas si está definido
+if (!empty($marca_llanta) || !empty($ancho_llanta) || !empty($alto_llanta) || !empty($rin_llanta)) {
+
+    $sql .= " LEFT JOIN historial_detalle_cambio hdc ON m.id = hdc.id_movimiento";
+    $sql .= " LEFT JOIN llantas ll ON ll.id = hdc.id_llanta LEFT JOIN marcas ma ON ll.Marca = ma.Imagen";
+    $sql .= " WHERE 1=1";
+    if(!empty($marca_llanta)){
+        $marcas_ids =  implode(",", array_map(function($valor) use ($con, $sanitizacion){
+            return $sanitizacion($valor, $con);
+        }, $marca_llanta));
+        // Agrega la operación INNER JOIN y la condición
+        $sql .= " AND ma.id IN (" . $marcas_ids . ")";
+    }
+    if(!empty($ancho_llanta) && $ancho_llanta != 'null'){
+        $sql .= " AND ll.Ancho = '" . $ancho_llanta . "'";
+    }
+    if(!empty($alto_llanta) && $ancho_llanta != 'null'){
+        $sql .= " AND ll.Proporcion = '" . $alto_llanta . "'";
+    }
+    if(!empty($rin_llanta) && $ancho_llanta != 'null'){
+        $sql .= " AND ll.Diametro = '" . $rin_llanta . "'";
+    }
+    
+}
+
+//SOLO TIPO INGRESO EN CUENTAS
+$sql .= " AND m.tipo = 2";
+
+
+// Filtra por fecha inicial y final si están definidas
+if (!empty($fecha_inicial) && !empty($fecha_final)) {
+    $fecha_inicial_ = $con->real_escape_string($fecha_inicial);
+    $fecha_final_ = $con->real_escape_string($fecha_final);
+    $sql .= " AND m.fecha BETWEEN '" . $fecha_inicial_ . "' AND '" . $fecha_final_ . "'";
+}else if (!empty($fecha_inicial)) {
+    $fecha_inicial_ = $con->real_escape_string($fecha_inicial);
+    $sql .= " AND m.fecha = '" . $fecha_inicial_ . "'";
+}else if (!empty($fecha_final)) {
+    $fecha_final_ = $con->real_escape_string($fecha_final);
+    $sql .= " AND m.fecha = '" . $fecha_final_ . "'";
+}
+
+// Filtra por sucursal si está definida
+if (!empty($sucursal_ubicacion)) {
+    $sucursal_ubicacion_ = $con->real_escape_string($sucursal_ubicacion);
+    $sql .= " AND hdc.id_ubicacion = '" . $sucursal_ubicacion_ . "'";
+}
+
+if (!empty($sucursal_destino)) {
+    $sucursal_destino_ = $con->real_escape_string($sucursal_destino);
+    $sql .= " AND hdc.id_destino = '" . $sucursal_destino_ . "'";
+}
+
+// Filtra por vendedor si está definido
+if (!empty($proveedor)) {
+    
+    $proveedores_ids =  implode(",", array_map(function($valor) use ($con, $sanitizacion){
+        return $sanitizacion($valor, $con);
+    }, $proveedor));
+    $sql .= " AND m.proveedor_id IN (" . $proveedores_ids. ")";
+}
+
+// Filtra por folio si está definido
+if (!empty($folio)) {
+    $folio_ = $con->real_escape_string($folio);
+    $sql .= " AND m.id = '" .$folio_ . "'";
+}
+
+// Filtra por folio si está definido
+if (!empty($factura)) {
+    $factura_ = $con->real_escape_string($factura);
+    $sql .= " AND m.folio_factura = '" .$factura_ . "'";
+}
+
+// Filtra por tipo de venta si está definido
+if (!empty($tipo)) {
+    $tipo_ids =  implode(",", array_map(function($valor) use ($con, $sanitizacion){
+        return $sanitizacion($valor, $con);
+    }, $tipo));
+    $sql .= " AND m.tipo IN (" . $tipo_ids .")";
+}
+
+// Filtra por estatus si está definido (múltiples valores)
+if (!empty($estatus)) {
+    $estatus_ids =  implode(",", array_map(function($valor) use ($con, $sanitizacion){
+        return $sanitizacion($valor, $con);
+    }, $estatus));
+    $sql .= " AND m.estatus IN (" . $estatus_ids .")";
+} 
+
+// Filtra por estatus si está definido (múltiples valores)
+if (!empty($estado_factura)) {
+    $estado_ids =  implode(",", array_map(function($valor) use ($con, $sanitizacion){
+        return $sanitizacion($valor, $con);
+    }, $estado_factura));
+    $sql .= " AND m.estado_factura IN (" . $estado_ids .")";
+} 
+//print_r($sql);
+//Ejecutamos la quert
+$sql .= " ORDER BY m.id DESC";
+/* print_r($sql);
+die(); */
+$stmt = $con->prepare($sql);
+$stmt->execute();
+$result = $stmt->get_result();
+$data = $result->fetch_all(MYSQLI_ASSOC);
+$stmt->close();
+
+$total_resultados = count($data);
+if($total_resultados >0){
+    $estatus = true;
+    $mensaje = 'Se encontrarón resultados';
+}else{
+    $data = [];
+    $estatus = true;
+    $mensaje = 'No se encontrarón resultados';
+}
+
+$res = array('query' => $sql, 'post'=> $_POST, 'data'=>$data, 'estatus'=>$estatus, 'mensaje'=>$mensaje, 'numero_resultados'=>$total_resultados);
+echo json_encode($res);
+?>
--- src/js/filtros.js
+++ src/js/filtros.js
@@ -153,7 +153,12 @@ function procesarFiltros(){
                 responsive: false,
                 ordering: "enable",
                 multiColumnSort: true,
-              });},500);
+              });
+            
+              $("table.dataTable thead").addClass("table-dark")
+              $("table.dataTable thead").addClass("text-white")
+            
+            },500);
           }else{
             tabla_ventas.empty()
             .append(`
@@ -415,7 +420,7 @@ function procesarFiltrosCuentasPorPagar(){
   //return false;
     $.ajax({
       type: "post",
-      url: "./modelo/filtros/aplicar-filtro-movimientos.php",
+      url: "./modelo/filtros/aplicar-filtro-cuentas.php",
       data: {folio, factura, fecha_final, fecha_inicial, sucursal_ubicacion, sucursal_destino, marca_llanta, ancho_llanta, alto_llanta, rin_llanta, filtro_estatus, filtro_tipo, filtro_proveedor, filtro_estado},
       dataType: "JSON",
       success: function (response) {
@@ -423,16 +428,18 @@ function procesarFiltrosCuentasPorPagar(){
               //Conversion de arreglo de objectos a arreglos de arrays
               response.data = response.data.length == 0 ? [] : response.data;
               const data_convertida = response.data.map(objeto => [
-                  '',
+                  null,
                   objeto.id,
-                  objeto.descripcion,
                   objeto.mercancia,
                   objeto.nombre,
                   objeto.folio_factura,
                   objeto.estado_factura,
+                  objeto.total,
+                  objeto.pagado,
+                  objeto.restante,
                   objeto.fecha,
+                  objeto.hora,
                   objeto.tipo,
-                  objeto.estatus,
                   objeto.usuario
               ]);
              
@@ -460,11 +467,10 @@ function procesarFiltrosCuentasPorPagar(){
                
                "bDestroy": true,
               columns: [   
-              { title: '#' },
-              { title: 'Folio'},
-              {title:  'Descripcion'},
-              { title: 'Mercancia'},
-              { title: 'proveedor',   render:(data, row)=>{
+                { title: "#",              data: null},
+                { title: "Folio",          data: 1 },
+                { title: "mercancia",      data: 2},
+                { title: 'proveedor',      data: 3, render:(data, row)=>{
                   if(data == null){
                     var prov = 'NA'
                   }else{
@@ -472,21 +478,33 @@ function procesarFiltrosCuentasPorPagar(){
                   }
                   return prov;
                 }},
-              { title: 'Factura'},
-              { title: 'Estado fact.', render(data, row) {
-                if(data == 1){
-                  estado_factura_ = 'Sin factura';
-                }else if(data == 2){
-                  estado_factura_ = 'Factura completa';
-                }else if(data == 3){
-                  estado_factura_ = 'Factura incompleta';
-                }else{
-                  estado_factura_ = 'No aplica'
-                }
-                return estado_factura_;
-              }},
-              { title: 'Fecha'},
-              { title: "Tipo", render(data, row) {
+                { title: 'factura',        data: 4},
+                { title: 'Estado fact.', data:5, render(data, row) {
+                  if(data == 1){
+                    estado_factura_ = 'Sin factura';
+                  }else if(data == 2){
+                    estado_factura_ = 'Factura completa';
+                  }else if(data == 3){
+                    estado_factura_ = 'Factura incompleta';
+                  }else if(data == 4){
+                    estado_factura_ = '<span class="badge bg-success p-2 text-white">Factura pagada</span>';
+                  }else{
+                    estado_factura_ = 'No aplica'
+                  }
+                  return estado_factura_;
+                }},
+                { title: "total",          data: 6, render: function(data){
+                    return new Intl.NumberFormat().format(data)
+                } },
+                { title: "pagado",          data: 7, render: function(data){
+                  return new Intl.NumberFormat().format(data)
+              } },
+              { title: "restante",          data: 8, render: function(data){
+                return new Intl.NumberFormat().format(data)
+            } },
+                { title: "fecha",          data: 9 },
+                { title: "hora",           data: 10 },
+                { title: "tipo",           data: 11, render(data, row) {
                   if(data == 1){
                     tipo = 'Movimiento';
                   }else if(data == 2){
@@ -502,47 +520,53 @@ function procesarFiltrosCuentasPorPagar(){
                   }
                   return tipo;
                 }},
-              { title: 'Estatus'},
-              { title: 'Usuario'},
-              { title: "Accion",
+                { title: "usuario",        data: 12 },
+                { title: "Accion",
                   data: null,
                   className: "celda-acciones",
                   render: function (row, data) {
-                      if(row[9]=='Completado'){
+                    if(row[11] =='patata'){
                       class_btn_check = 'btn-secondary disabled';
                       candado ='ss';
-                      }else{
+                    }else{
                       candado = '';
-                      class_btn_check = 'btn-success';
-                      }
-                      if(row[8] == 1 || row[8] ==3 || row[8] ==4){
+                      class_btn_check = 'btn-primary';
+                    }
+                    if(row[11] == 1 || row[6] ==3 || row[6] ==4){
                       return `
                       <div style="display:flex;">
-                          <div class="btn btn-danger mr-2" onclick="remisionSalida(${row[1]})"><i class="fas fa-file-pdf"></i></div>
-                          <div class="btn ${class_btn_check}" onclic${candado}k="AprobarMovimiento(${row[1]})"><i class="fas fa-check" disabled></i></div>
+                          <div class="btn btn-danger mr-2" onclick="remisionSalida(${row[0]})"><i class="fas fa-file-pdf"></i></div>
+                          <div class="btn ${class_btn_check}" onclic${candado}k="administrarCuenta(${row[1]}, ${row[11]})"><i class="fas fa-check" disabled></i></div>
                       </div>
                           `;
-                      }else if(row[8] ==2){
+                    }else if(row[11] ==2){
                       return `
                       <div style="display:flex;">
-                          <div class="btn btn-danger mr-2" onclick="remisionIngreso(${row[1]})"><i class="fas fa-file-pdf"></i></div>
-                          <div class="btn ${class_btn_check}" onclick="AprobarMovimiento(${row[1]})"><i class="fas fa-check"></i></div>
+                         <div class="btn btn-danger mr-2" onclick="remisionIngreso(${row[0]})"><i class="fas fa-file-pdf"></i></div>
+                         <div class="btn ${class_btn_check}" onclick="administrarCuenta(${row[1]},  ${row[11]})"><i class="fas fa-check"></i></div>
                       </div>`;
-
-                      }else{
+            
+                    }else{
                       return `<span>No disp</span>`;
-                      }
+                    }
                   },
-                  }, 
+                }, 
               ],
+              columnDefs:[{
+                width: '50px',
+                targets:0
+              }],
               data: data_convertida,
               paging: true,
               searching: true,
               scrollY: "50vh",
               info: false,
-              responsive: false,
               order: [1, "desc"],
-            });},500);
+            });
+          
+            $("table.dataTable thead").addClass("table-dark")
+            $("table.dataTable thead").addClass("text-white")
+          },500);
         }else{
           var tabla = $('#cuentas-por-pagar');
           tabla.empty()
@@ -558,6 +582,7 @@ function procesarFiltrosCuentasPorPagar(){
               `
           })
         }
+        
       }
   });
   
